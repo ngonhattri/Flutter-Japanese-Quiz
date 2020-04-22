911485ac1a26906b0558e9a3718bfdc7bd26b401
nhat tri <nhattri1503@gmail.com>
2020-04-23 01:41:12 +0700

    completed full test with score

--- lib/resources/api_provider.dart
+++ lib/resources/api_provider.dart
@@ -7,12 +7,14 @@ import 'package:flutter_jpn_ocr/models/question.dart';
 //const String baseUrl = "https://opentdb.com/api.php";
 const String baseUrl = "https://tri-ngo.herokuapp.com";
 
-Future<List<Question>> getQuestions(Category category, int total, String difficulty) async {
-  String url = "$baseUrl?amount=$total&category=${category.id}";
-  if(difficulty != null) {
+Future<List<Question>> getQuestions(
+    Category category, int total, String difficulty) async {
+  String url = "$baseUrl?amount=$total&category=${category.name}";
+  if (difficulty != null) {
     url = "$url&difficulty=$difficulty";
   }
   http.Response res = await http.get(url);
-  List<Map<String, dynamic>> questions = List<Map<String,dynamic>>.from(json.decode(res.body)["data"]);
+  List<Map<String, dynamic>> questions =
+      List<Map<String, dynamic>>.from(json.decode(res.body)["data"]);
   return Question.fromData(questions);
 }
--- lib/ui/pages/quiz_page.dart
+++ lib/ui/pages/quiz_page.dart
@@ -12,7 +12,8 @@ class QuizPage extends StatefulWidget {
   final List<Question> questions;
   final Category category;
 
-  const QuizPage({Key key, @required this.questions, this.category}) : super(key: key);
+  const QuizPage({Key key, @required this.questions, this.category})
+      : super(key: key);
 
   @override
   _QuizPageState createState() => _QuizPageState();
@@ -21,53 +22,28 @@ class QuizPage extends StatefulWidget {
 class _QuizPageState extends State<QuizPage> {
   List<Offset> points = List();
   AppBrain brain = AppBrain();
-  
-String headerText = 'Header placeholder';
-  String footerText = 'Footer placeholder';
-  double accuracy = 0.58;
-
-  
-
-  void _resetLabels() {
-    headerText = kWaitingForInputHeaderString;
-    footerText = kWaitingForInputFooterString;
-    accuracy = 0.00;
-  }
-
-  void _setLabelsForGuess(String guess,double acc) {
-    headerText = ""; // Empty string
-    accuracy = acc*100;
-    footerText = kGuessingInputString + guess + "-" + accuracy.toInt().toString() + "%";
-  }
-  
-  
 
   void _cleanDrawing() {
     setState(() {
       points = List();
     });
   }
-  
+
   final TextStyle _questionStyle = TextStyle(
-    fontSize: 18.0,
-    fontWeight: FontWeight.w500,
-    color: Colors.white
-  );
+      fontSize: 18.0, fontWeight: FontWeight.w500, color: Colors.white);
 
   int _currentIndex = 0;
-  final Map<int,dynamic> _answers = {};
+  final Map<int, dynamic> _answers = {};
   final GlobalKey<ScaffoldState> _key = GlobalKey<ScaffoldState>();
 
   @override
   void initState() {
     super.initState();
     brain.loadModel();
-    _resetLabels();
   }
 
   @override
-  Widget build(BuildContext context){
-    
+  Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: _onWillPop,
       child: Scaffold(
@@ -81,9 +57,8 @@ String headerText = 'Header placeholder';
             ClipPath(
               clipper: WaveClipperTwo(),
               child: Container(
-                decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor
-                ),
+                decoration:
+                    BoxDecoration(color: Theme.of(context).primaryColor),
                 height: 200,
               ),
             ),
@@ -95,67 +70,72 @@ String headerText = 'Header placeholder';
                     children: <Widget>[
                       CircleAvatar(
                         backgroundColor: Colors.white70,
-                        child: Text("${_currentIndex+1}"),
+                        child: Text("${_currentIndex + 1}"),
                       ),
                       SizedBox(width: 16.0),
                       Expanded(
-                        child: Text(HtmlUnescape().convert(widget.questions[_currentIndex].question),
+                        child: Text(
+                          HtmlUnescape().convert(
+                              widget.questions[_currentIndex].question),
                           softWrap: true,
-                          style: _questionStyle,),
+                          style: _questionStyle,
+                        ),
                       ),
                     ],
                   ),
-
                   Container(
-              decoration: new BoxDecoration(
-                border: new Border.all(
-                  width: 3.0,
-                  color: Colors.blue,
-                ),
-              ),
-              child: Builder(
-                builder: (BuildContext context) {
-                  return GestureDetector(
-                    onPanUpdate: (details) {
-                      setState(() {
-                        RenderBox renderBox = context.findRenderObject();
-                        points.add(
-                            renderBox.globalToLocal(details.globalPosition));
-                      });
-                    },
-                    onPanStart: (details) {
-                      setState(() {
-                        RenderBox renderBox = context.findRenderObject();
-                        points.add(
-                            renderBox.globalToLocal(details.globalPosition));
-                      });
-                    },
-                    onPanEnd: (details) async {
-                      points.add(null);
-                      List predictions = await brain.processCanvasPoints(points);
-                      print(predictions);
-                      setState(() {
-                        _setLabelsForGuess(predictions.first['label'], predictions.first['confidence']);
-                        _answers[_currentIndex] = predictions.first['label'];
-                      });
-                    },
-                    child: ClipRect(
-                      child: CustomPaint(
-                        size: Size(kCanvasSize, kCanvasSize),
-                        painter: DrawingPainter(
-                          offsetPoints: points,
-                        ),
+                    decoration: new BoxDecoration(
+                      border: new Border.all(
+                        width: 3.0,
+                        color: Colors.blue,
                       ),
                     ),
-                  );
-                },
-              ),
-            ),
+                    child: Builder(
+                      builder: (BuildContext context) {
+                        return GestureDetector(
+                          onPanUpdate: (details) {
+                            setState(() {
+                              RenderBox renderBox = context.findRenderObject();
+                              points.add(renderBox
+                                  .globalToLocal(details.globalPosition));
+                            });
+                          },
+                          onPanStart: (details) {
+                            setState(() {
+                              RenderBox renderBox = context.findRenderObject();
+                              points.add(renderBox
+                                  .globalToLocal(details.globalPosition));
+                            });
+                          },
+                          onPanEnd: (details) async {
+                            points.add(null);
+                            List predictions =
+                                await brain.processCanvasPoints(points);
+                            print(predictions);
+
+                            _answers[_currentIndex] =
+                                predictions.first['label'];
+                          },
+                          child: ClipRect(
+                            child: CustomPaint(
+                              size: Size(kCanvasSize, kCanvasSize),
+                              painter: DrawingPainter(
+                                offsetPoints: points,
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
                   Expanded(
                     child: Container(
                       alignment: Alignment.bottomCenter,
                       child: RaisedButton(
-                        child: Text( _currentIndex == (widget.questions.length - 1) ? "Nộp" : "Tiếp theo"),
+                        child: Text(
+                            _currentIndex == (widget.questions.length - 1)
+                                ? "Nộp"
+                                : "Tiếp theo"),
                         onPressed: _nextSubmit,
                       ),
                     ),
@@ -166,64 +146,58 @@ String headerText = 'Header placeholder';
           ],
         ),
         floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _cleanDrawing();
-          _resetLabels();
-        },
-        tooltip: 'Clean',
-        child: Icon(Icons.delete),
-      ),
+          onPressed: () {
+            _cleanDrawing();
+          },
+          tooltip: 'Clean',
+          child: Icon(Icons.delete),
+        ),
       ),
     );
   }
 
   void _nextSubmit() {
-    if(points.isEmpty == true) {
+    if (points.isEmpty == true) {
       _key.currentState.showSnackBar(SnackBar(
         content: Text("You must write an answer to continue."),
       ));
       return;
-    } else {
-      _key.currentState.showSnackBar(SnackBar(
-        content: Text(footerText),
-      ));
     }
-    if(_currentIndex < (widget.questions.length - 1)){
+    if (_currentIndex < (widget.questions.length - 1)) {
       setState(() {
-          _currentIndex++;
+        _currentIndex++;
       });
       _cleanDrawing();
-      _resetLabels();
     } else {
       Navigator.of(context).pushReplacement(MaterialPageRoute(
-        builder: (_) => QuizFinishedPage(questions: widget.questions, answers: _answers)
-      ));
+          builder: (_) => QuizFinishedPage(
+              questions: widget.questions, answers: _answers)));
     }
   }
 
   Future<bool> _onWillPop() async {
     return showDialog<bool>(
-      context: context,
-      builder: (_) {
-        return AlertDialog(
-          content: Text("Bạn có chắc muốn thoát? Toàn bộ quá trình sẽ bị mất dữ liệu."),
-          title: Text("Cảnh báo!"),
-          actions: <Widget>[
-            FlatButton(
-              child: Text("Có"),
-              onPressed: (){
-                Navigator.pop(context,true);
-              },
-            ),
-            FlatButton(
-              child: Text("Không"),
-              onPressed: (){
-                Navigator.pop(context,false);
-              },
-            ),
-          ],
-        );
-      }
-    );
+        context: context,
+        builder: (_) {
+          return AlertDialog(
+            content: Text(
+                "Bạn có chắc muốn thoát? Toàn bộ quá trình sẽ bị mất dữ liệu."),
+            title: Text("Cảnh báo!"),
+            actions: <Widget>[
+              FlatButton(
+                child: Text("Có"),
+                onPressed: () {
+                  Navigator.pop(context, true);
+                },
+              ),
+              FlatButton(
+                child: Text("Không"),
+                onPressed: () {
+                  Navigator.pop(context, false);
+                },
+              ),
+            ],
+          );
+        });
   }
 }
